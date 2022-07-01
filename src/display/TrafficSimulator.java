2ec04e00f4147c008c517dee9f6f210c17d3fffd
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2022-07-01 15:55:37 +0500

    added some ui elements

--- src/display/TrafficSimulator.java
+++ src/display/TrafficSimulator.java
@@ -1,6 +1,7 @@
 package display;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TrafficSimulator extends JFrame{
     private JPanel mainPanel;
@@ -16,6 +17,8 @@ public class TrafficSimulator extends JFrame{
     private JButton roundAboutButton;
     private JButton signalButton;
 
+    private JPanel map_frame;
+
     public TrafficSimulator(int width, int height){
 
         this.setTitle("Welcome");
@@ -23,6 +26,21 @@ public class TrafficSimulator extends JFrame{
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
 
+        JPanel map = new JPanel();
+        map.setBounds(0,0,200,200);
+        map.setLayout(new GridLayout(2,2));
+        map_frame.add(map);
+        for(int i=0; i<9;i++ ) {
+
+            JButton map_tile = new JButton("Grass");
+            map_tile.setBounds(0,0,100,100);
+            map_tile.setPreferredSize(new Dimension(100, 100));
+            map_tile.addActionListener(e -> {
+                System.out.println("You added a road");
+            });
+            map.add(map_tile);
+
+        }
 
     }
 

eccc4459172110f28ad1cbb8d45387afa43fd164
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2022-07-01 16:19:27 +0500

    added some ui elements

--- src/TrafficSimulator.java
+++ src/TrafficSimulator.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TrafficSimulator extends JFrame{
     private JPanel mainPanel;
@@ -8,6 +9,7 @@ public class TrafficSimulator extends JFrame{
     private JButton simulateButton;
     private JButton OptionsButton;
     private JLabel mainHeading;
+    private JPanel map_frame;
     private JButton roadButton;
     private JButton tunnelButton;
     private JButton bridgeButton;
@@ -21,6 +23,21 @@ public class TrafficSimulator extends JFrame{
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
 
+        JPanel map = new JPanel();
+        map.setBounds(0,0,200,200);
+        map.setLayout(new GridLayout(2,2));
+        map_frame.add(map);
+        for(int i=0 ; i<9 ; i++ ) {
+
+            JButton map_tile = new JButton("Grass");
+            map_tile.setBounds(0,0,100,100);
+            map_tile.setPreferredSize(new Dimension(100, 100));
+            map_tile.addActionListener(e -> {
+                System.out.println("You added a road");
+            });
+            map.add(map_tile);
+
+        }
 
     }
 

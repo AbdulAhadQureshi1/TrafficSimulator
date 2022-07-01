54ae9cd088b7958fe617db9e57a80b0da8d4fa27
Syed Arsal Rahman <arsalazfar@hotmail.com>
2022-07-01 15:37:39 +0500

    Added to sql

--- src/database/Sql.java
+++ src/database/Sql.java
@@ -1,7 +1,7 @@
 package database;
 import java.sql.*;
 import java.lang.Integer;
-
+import java.util.Date;
 public class Sql {
     private final String DB_URL = "jdbc:mysql://localhost/trafficsim";
     private final String USER = "root";
@@ -35,27 +35,33 @@ public class Sql {
             return 1;
         }
     }
-    public boolean authetication(String username, String password){
+    public int authetication(String username, String password){
         int passhash = password.hashCode();
         try{
             ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM Login_Details WHERE UserName = '" + username + "' AND Password = " + passhash + ");");
             while(rs.next()) {
                 int rowCount = Integer.parseInt(rs.getString("COUNT(*)"));
-                if (rowCount == 0) {
-                    return false;
-                } else {
-                    return true;
+                if (rowCount != 0) {
+                    rs = stmt.executeQuery("SELECT Serial FROM Login_Details WHERE UserName = '" + username + "' AND Password = " + passhash + ");");
+                    while (rs.next()) {
+                        return rs.getInt("Serial");
+                    }
                 }
             }
         } catch(SQLException e) {
             e.printStackTrace();
-            return true;
         }
-        return false;
+        return -1;
     }
-    //public int newSimulation(){
-    //  pass;
-    //}
+    public int newSimulation(int entrySerial,int trafficFlow, int accidents, float efficiency, float avg, int userSerial){
+        Date date = new Date();
+        try{
+            return stmt.executeUpdate("INSERT INTO sim_record VALUES (" + entrySerial +","+ trafficFlow + "," + accidents + "," + efficiency + "," + avg + "," + date.toString() +","+ userSerial + ");");
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
     public static void main(String[] args){
         Sql db = new Sql();
         System.out.println(db.newUser("Arz","bbg"));
